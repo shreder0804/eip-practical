f5b3085237dd9a746bbf9c707c7b5bcfe3031c87
shreder0804 <shreder0804@gmail.com>
2022-05-22 19:36:45 +0900

    Add : dataType.c

---
+++ dataType.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  // 데이터타입 정의
+  int i = 3;
+  double d = 5;
+  float t = 2.32;
+  char c = 'A';
+
+  printf("%d\n", i);
+  printf("%d\n", d);
+  printf("%f\n", t);
+  printf("%c\n", c);
+
+  return 0;
+}
