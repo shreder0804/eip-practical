ef6568b3fc19b2a2ef188256cbdbc1206ba65a46
shreder0804 <shreder0804@gmail.com>
2022-05-29 22:33:15 +0900

    Add : Array.java

---
+++ Array.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Array {
+
+  public static void main(String[] args) {
+    int[] a = new int[3];
+    int[] b = { 1, 2, 3 };
+
+    System.out.println(a.length);
+    System.out.println(Arrays.toString(b));
+  }
+}
