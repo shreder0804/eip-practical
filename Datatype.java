662ab48812e2cf9fb57a3431892498f89e2cc210
shreder0804 <shreder0804@gmail.com>
2022-05-30 22:58:13 +0900

    Add : Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype {
+
+  public static void main(String[] args) {
+    String s = "Hello";
+    int i = 3;
+    double d = 2.2;
+    char c = 'B';
+    boolean b = true;
+
+    System.out.println(s);
+    System.out.println(i);
+    System.out.println(d);
+    System.out.println(c);
+    System.out.println(b);
+  }
+}
