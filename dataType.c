b10290286a608a917621989a36e5dd9eab2b545f
shreder0804 <shreder0804@gmail.com>
2022-05-30 23:04:05 +0900

    Fix : Add 'char []' datatype

--- dataType.c
+++ dataType.c
@@ -7,11 +7,13 @@ int main(int argc, char *argv[])
   double d = 5;
   float t = 2.32;
   char c = 'A';
+  char s[] = "Hello";
 
   printf("%d\n", i);
   printf("%d\n", d);
   printf("%f\n", t);
   printf("%c\n", c);
+  printf("%s", s);
 
   return 0;
 }
