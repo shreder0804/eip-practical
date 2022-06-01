f48340eaf1c985341c588f9ff03ca2c48dbe7120
shreder0804 <shreder0804@gmail.com>
2022-06-01 23:12:09 +0900

    Fix : Add printf line

--- pointer.c
+++ pointer.c
@@ -6,7 +6,10 @@ int main(int argc, char *argv[])
   int *b = &a;
 
   // *를 붙여야 값이 출력됨
-  printf("%d", *b);
+  printf("%d\n", *b); // 포인터 b가 가리키는 a의 값
+  printf("%d\n", &a); // a의 주소값
+  printf("%d\n", b);  // 포인터 b가 가리키는 a의 주소값
+  printf("%d\n", &b); // 포인터 b의 주소값
 
   return 0;
 }
