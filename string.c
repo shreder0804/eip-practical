fe8d631e7f28bd5eb6d0047559236f8e1c26cbd9
shreder0804 <shreder0804@gmail.com>
2022-06-02 13:02:15 +0900

    Fix : Add comment

--- string.c
+++ string.c
@@ -12,14 +12,24 @@ int main(int argc, char *argv[])
 
   printf("%s\n", a + 1);
 
-  a[4] = NULL;
+  /*
+
+  char 자료형에 b[5] = NULL; 이라고 넣게 되면
+
+  assignment makes integer from pointer without a cast라는 warining이 뜨게 된다.
+  컴파일러가 NULL을 (void*)0 으로 인식하는데 말그대로 포인터가 casting없이 인티저로 된다는 것 같다.
+
+  b[5] = '\0'; 로 입력하면 warning 문구가 사라진다.
+
+  */
+  a[4] = '\0';
 
   printf("%s\n", a + 1);
 
   printf("%s\n", b[0]);
   printf("%s\n", b[1]);
 
-  b[0][4] = NULL;
+  b[0][4] = '\0';
 
   printf("%s\n", b[0] + 2);
   printf("%s\n", b[1] + 3);
