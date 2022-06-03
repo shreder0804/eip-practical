e7bdfff1fb20f4ba98498ec9d27e9b055661849f
shreder0804 <shreder0804@gmail.com>
2022-06-03 12:57:19 +0900

    Add : identifierNotation.py

---
+++ identifierNotation.py
@@ -0,0 +1,18 @@
+# 식별자 표기법은 크게 4가지가 있다
+
+# 1. 카멜 표기법
+inputFunction = 1
+
+# 2. 파스칼 표기법
+InputFunction = 2
+
+# 3. 스네이크 표기법
+input_fuction = 3
+
+# 4. 헝가리안 표기법
+nScore = 4
+
+print(inputFunction)
+print(InputFunction)
+print(input_fuction)
+print(nScore)
